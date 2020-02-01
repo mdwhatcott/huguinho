c9e2d2bde1fed1575c6a335fcc4cdc0b82f71f71
Michael Whatcott <mdwhatcott@gmail.com>
2020-02-01 15:42:46 -0700

    File system interfaces.

--- contracts/fs.go
+++ contracts/fs.go
@@ -1,6 +1,54 @@
 package contracts
 
+import (
+	"io"
+	"os"
+	"path/filepath"
+)
+
 type (
 	Path string
 	File string
+)
+
+type (
+	Stat interface {
+		Stat(path string) (os.FileInfo, error)
+	}
+
+	Open interface {
+		Open(path string) (io.ReadCloser, error)
+	}
+
+	Create interface {
+		Create(path string) (io.WriteCloser, error)
+	}
+
+	ReadFile interface {
+		ReadFile(path string) ([]byte, error)
+	}
+
+	WriteFile interface {
+		WriteFile(path string, content []byte, perm os.FileMode) error
+	}
+
+	Mkdir interface {
+		Mkdir(path string, perm os.FileMode) error
+	}
+
+	MkdirAll interface {
+		MkdirAll(path string, perm os.FileMode) error
+	}
+
+	Remove interface {
+		Remove(path string) error
+	}
+
+	RemoveAll interface {
+		RemoveAll(path string) error
+	}
+
+	Walk interface {
+		Walk(root string, walk filepath.WalkFunc) error
+	}
 )
