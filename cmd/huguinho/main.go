a8d7e612e790df4071b41de0727b4a64dac3a2cc
Michael Whatcott <mdwhatcott@gmail.com>
2020-06-21 11:01:11 -0600

    Moved disk to separate package.

--- cmd/huguinho/main.go
+++ cmd/huguinho/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
 
 	"github.com/mdwhatcott/huguinho/core"
+	"github.com/mdwhatcott/huguinho/io"
 )
 
 func init() {
@@ -14,29 +13,5 @@ func init() {
 }
 
 func main() {
-	os.Exit(core.NewPipelineRunner(os.Args[1:], NewDisk()).Run())
-}
-
-////////////////////////////////////////////////////////////
-
-type Disk struct{}
-
-func NewDisk() *Disk {
-	return new(Disk)
-}
-
-func (this *Disk) ReadFile(path string) ([]byte, error) {
-	return ioutil.ReadFile(path)
-}
-
-func (this *Disk) WriteFile(path string, content []byte, perm os.FileMode) error {
-	return ioutil.WriteFile(path, content, perm)
-}
-
-func (this *Disk) MkdirAll(path string, perm os.FileMode) error {
-	return os.MkdirAll(path, perm)
-}
-
-func (this *Disk) Walk(root string, walk filepath.WalkFunc) error {
-	return filepath.Walk(root, walk)
+	os.Exit(core.NewPipelineRunner(os.Args[1:], io.NewDisk()).Run())
 }
