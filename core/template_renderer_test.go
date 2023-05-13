6ee78c2def46a228e6a517c6b5c47f2fb35b09e1
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2023-05-13 00:00:13 -0600

    Extract method to eliminate duplication.

--- core/template_renderer_test.go
+++ core/template_renderer_test.go
@@ -21,66 +21,43 @@ type TemplateRendererFixture struct {
 }
 
 func (this *TemplateRendererFixture) Setup() {
-	this.parseHomePageTemplate()
-	this.parseArticleTemplate()
-	this.parseTopicsTemplate()
+	this.parseTemplate(contracts.HomePageTemplateName)
+	this.parseTemplate(contracts.ArticleTemplateName)
+	this.parseTemplate(contracts.TopicsTemplateName)
 	this.renderer = NewTemplateRenderer(this.templates)
 	this.So(this.renderer.Validate(), should.BeNil)
 }
-
-func (this *TemplateRendererFixture) parseTopicsTemplate() {
+func (this *TemplateRendererFixture) parseTemplate(name string) {
 	var err error
 	if this.templates == nil {
-		this.templates = template.New(contracts.TopicsTemplateName)
+		this.templates = template.New(name)
 	} else {
-		this.templates = this.templates.New(contracts.TopicsTemplateName)
+		this.templates = this.templates.New(name)
 	}
-	this.templates, err = this.templates.Parse(contracts.TopicsTemplateName)
-	this.So(err, should.BeNil)
-}
-
-func (this *TemplateRendererFixture) parseArticleTemplate() {
-	var err error
-	if this.templates == nil {
-		this.templates = template.New(contracts.ArticleTemplateName)
-	} else {
-		this.templates = this.templates.New(contracts.ArticleTemplateName)
-	}
-	this.templates, err = this.templates.Parse(contracts.ArticleTemplateName)
-	this.So(err, should.BeNil)
-}
-
-func (this *TemplateRendererFixture) parseHomePageTemplate() {
-	var err error
-	if this.templates == nil {
-		this.templates = template.New(contracts.HomePageTemplateName)
-	} else {
-		this.templates = this.templates.New(contracts.HomePageTemplateName)
-	}
-	this.templates, err = this.templates.Parse(contracts.HomePageTemplateName)
+	this.templates, err = this.templates.Parse(name)
 	this.So(err, should.BeNil)
 }
 
 func (this *TemplateRendererFixture) TestMissingHomePageTemplate_ValidateErr() {
 	this.templates = nil
-	this.parseArticleTemplate()
-	this.parseTopicsTemplate()
+	this.parseTemplate(contracts.ArticleTemplateName)
+	this.parseTemplate(contracts.TopicsTemplateName)
 	this.renderer = NewTemplateRenderer(this.templates)
 	this.So(this.renderer.Validate(), should.NOT.BeNil)
 }
 
 func (this *TemplateRendererFixture) TestMissingTopicsTemplate_ValidateErr() {
 	this.templates = nil
-	this.parseArticleTemplate()
-	this.parseHomePageTemplate()
+	this.parseTemplate(contracts.ArticleTemplateName)
+	this.parseTemplate(contracts.HomePageTemplateName)
 	this.renderer = NewTemplateRenderer(this.templates)
 	this.So(this.renderer.Validate(), should.NOT.BeNil)
 }
 
 func (this *TemplateRendererFixture) TestMissingArticleTemplate_ValidateErr() {
 	this.templates = nil
-	this.parseHomePageTemplate()
-	this.parseTopicsTemplate()
+	this.parseTemplate(contracts.HomePageTemplateName)
+	this.parseTemplate(contracts.TopicsTemplateName)
 	this.renderer = NewTemplateRenderer(this.templates)
 	this.So(this.renderer.Validate(), should.NOT.BeNil)
 }
